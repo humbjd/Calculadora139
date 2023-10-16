ac3d3b7ca0d2b52f996830193fa841b830f16ed4
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-10-16 11:02:59 -0300

    Conclusão do codigo

--- Calculadora.cs
+++ Calculadora.cs
@@ -12,7 +12,7 @@ public class Calculadora
 
 
     // 3.2 - Funções e Métodos
-    public int somarDoisNumeros(int num1, int num2)
+    public static int somarDoisNumeros(int num1, int num2)
     {
         int resultado = num1 + num2;
         //  resultado = 1    +     2
@@ -21,29 +21,29 @@ public class Calculadora
 
     } // fim da função somarDoisNumeros
 
-    public int subtrairDoisNumeros(int num1, int num2)
+    public static int subtrairDoisNumeros(int num1, int num2)
     {
         return num1 - num2;
     } // fim da função subtrairDoisNumeros
 
-    public int multiplicarDoisNumeros(int num1, int num2)
+    public static int multiplicarDoisNumeros(int num1, int num2)
     {
         return num1 * num2;
     } // fim da função multiplicarDoisNumeros
 
-    public int dividirDoisNumeros(int num1, int num2)
+    public static int dividirDoisNumeros(int num1, int num2)
     {
         return num1 / num2;
     } // fim da função dividirDoisNumeros
 
-    public void main()
+    public  static void Main()
     {
         // chamar as 4 operações
         Console.WriteLine("5 + 7 = " + somarDoisNumeros(5,7));
         Console.WriteLine("5 - 7 = " + subtrairDoisNumeros(5,7));
         Console.WriteLine("5 * 7 = " + multiplicarDoisNumeros(5,7));
-        Console.WriteLine("5 / 7 = " + dividirDoisNumeros(5,7));
-        
+        Console.WriteLine("35 / 7 = " + dividirDoisNumeros(35,7));
+
     }
 
 
